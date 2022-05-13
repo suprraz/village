13d700fa8098ba9e02e2e880c86450cd8764950a
suprraz <yo@suprovici.com>
2022-05-13 13:47:46 -0700

    enable tcp TURN

--- js/config.js
+++ js/config.js
@@ -33,11 +33,11 @@ export default {
       //   username: "openrelayproject",
       //   credential: "openrelayproject",
       // },
-      // {
-      //   urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      //   username: "openrelayproject",
-      //   credential: "openrelayproject",
-      // },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
     ]
   }
 }
