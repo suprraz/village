eee501c697e16aee31f9e69a20baa693ab2d48a6
suprraz <yo@suprovici.com>
2022-07-07 14:48:31 -0700

    Remove sandbox attributes

--- js/os/sandbox.js
+++ js/os/sandbox.js
@@ -79,7 +79,7 @@ const sandboxHtml = `
 <div id="overlayContainer" class="is-overlay hero">   
     <div class="hero-body">
       <button class="modal-close is-large" aria-label="close"></button>
-      <iframe sandbox="allow-downloads allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"
+      <iframe sandbox="allow-forms allow-modals allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"
         height="100%" width="100%" src="about:blank" name="sandboxIframe"
         id="sandboxIframe" srcdoc="
           <!DOCTYPE html>
