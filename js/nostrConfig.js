72b66033c2bb03e1fd83aa48672e3c80c26a3123
Raz <>
2023-03-05 16:59:48 -0800

    Changed default nostr relay

--- js/nostrConfig.js
+++ js/nostrConfig.js
@@ -2,11 +2,10 @@ export default {
   parallelReqs: 6,
   tryNewRelayTimeout: 300,
   relays: [
-    "wss://nostr-pub.wellorder.net",
-    // 'wss://nos.lol',
+    // "wss://nos.lol",
     // 'wss://eden.nostr.land',
     // ' wss://relay.nostr.info',
-    // 'wss://brb.io',
+    "wss://brb.io",
     // 'wss://relay.snort.social'
   ],
 };
